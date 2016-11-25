073f2995d6f6641ad028373a6fe73ae8779a0470
celestetorres <celeste.torres1902@gmail.com>
2016-11-25 15:30:30 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,12 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
-{
-	
-}
+{var nombre; //iniciación simple de una variable
+	nombre="Claudio";
+var apellido="Eberle";
+nombre=prompt("Ingrese su nombre","NN") //el NN es dato sugerido
+alert("Mi nombre es "+nombre +" y mi apellido es "+ apellido); 
 
+
+
+}
